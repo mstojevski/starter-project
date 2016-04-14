72f6c0832ddcc55ac0d38e2a7da6097b8f79104b
Milan Stojevski <Milan Stojevski>
2016-04-14 17:10:42 +0200

    all files added

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+//Settings for autoprefixer
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+
+gulp.task('sass', function () {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
+});
+gulp.task('serve', function() {
+    browserSync.init({
+        server: "./"
+    });
+});
+
+gulp.task('watch', function () {
+  gulp.watch('scss/**/*.scss', ['sass']);
+  gulp.watch('./index.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['sass', 'watch', 'serve']);
